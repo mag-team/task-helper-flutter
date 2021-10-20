58440d88848d358aa5dc71ebb4990ee039c3b4ae
Jakob Kordež <jakobkordez1999@gmail.com>
2021-10-20 21:34:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# task_helper
+
+A Kanban-style task manager
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
